172812469aa4d81da1969b1475ff6e56be212fe5
Eduardo Haag <eduardof.haag@gmail.com>
2023-08-24 10:17:23 -0300

    feat: criado estrutura básica modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,43 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
-const maxRecords = 151
+const maxRecord = 151
 const limit = 10
-let offset = 0;
+let offset = 0 
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `
-}
 
 function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
+    pokeApi.getPokemons(offset, limit).then((pokemons = [])=> {
+    const newPokemonList = pokemons.map((pokemon) => `
+      <li class="pokemon ${pokemon.type}" onClick="pokemonModal.show('${pokemon.name}')">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+            <img src="${pokemon.photo}" alt="${pokemon.name}" />
+        </div> 
+      </li>
+    `).join('')
+
+    pokemonList.innerHTML += newPokemonList
+  })
 }
 
 loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+  offset += limit
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+  const countRecordinTheNextPage = offset + limit
 
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
-    } else {
-        loadPokemonItens(offset, limit)
-    }
+  if(countRecordinTheNextPage >= maxRecord){
+    const newLimit = maxRecord - offset
+    loadPokemonItens(offset, newLimit)
+
+    loadMoreButton.parentElement.removeChild(loadMoreButton)
+  } else {
+    loadPokemonItens(offset, limit)
+  }  
 })
---
+++ assets/js/pokemon-modal.js
@@ -0,0 +1,4 @@
+const pokemonModal = {}
+const modal = document.querySelector('dialog')
+
+pokemonModal.show = (pokemonName) => modal.showModal()
